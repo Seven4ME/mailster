7ca1eeed8d8a286e62de20c657dcaf38fd6c81bc
Yaroslav Limanskiy <kobun2341@gmail.com>
2020-04-24 15:13:04 +0300

    Models fixes

--- mailster/email_import/models.py
+++ mailster/email_import/models.py
@@ -1,32 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Campaign(models.Model):
+    campaign_name = models.CharField(max_length=255),
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Contacts(models.Model):
     email = models.EmailField(),
-    created_at = models.DateField(),
-    isValid = models.BooleanField(),
-    campaign_name = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True),
+    is_valid = models.BooleanField(),
+    campaign_name = models.ForeignKey(Campaign, on_delete=models.CASCADE)
 
-class Last_sending(models.Model):
+class LastSending(models.Model):
     contacts = models.ForeignKey(Contacts, on_delete=models.CASCADE)
     email = models.EmailField(),
     last_email = models.DateField()
 
-class Campaigns(models.Model):
-    campaign_name = models.CharField(max_length=255),
-    user_id = models.IntegerField(),
-    template_name = models.CharField(max_length=255)
 
 class Statistic(models.Model):
-    campaigns = models.ForeignKey(Campaigns, on_delete=models.CASCADE),
     campaign_name = models.CharField(max_length=255),
     isOpened = models.BooleanField(),
     emails_count = models.IntegerField(),
     date = models.DateField()
 
 class Templates(models.Model):
-    campaigns = models.ForeignKey(Campaigns, on_delete=models.CASCADE),
-    template_name = models.CharField(max_length=255)
+    campaigns = models.ForeignKey(Campaign, on_delete=models.CASCADE),
+    template_name = models.CharField(max_length=255),
+    email_text = models.TextField(default="")
 
 
 
