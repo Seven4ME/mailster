d564a84a7f131fb6756c5ba6d7283606134803de
Yaroslav Limanskiy <kobun2341@gmail.com>
2020-04-24 12:58:09 +0300

    DB models

--- mailster/email_import/models.py
+++ mailster/email_import/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Contacts(models.Model):
+    email = models.EmailField(),
+    created_at = models.DateField(),
+    isValid = models.BooleanField(),
+    campaign_name = models.CharField(max_length=255)
+
+class Last_sending(models.Model):
+    contacts = models.ForeignKey(Contacts, on_delete=models.CASCADE)
+    email = models.EmailField(),
+    last_email = models.DateField()
+
+class Campaigns(models.Model):
+    campaign_name = models.CharField(max_length=255),
+    user_id = models.IntegerField(),
+    template_name = models.CharField(max_length=255)
+
+class Statistic(models.Model):
+    campaigns = models.ForeignKey(Campaigns, on_delete=models.CASCADE),
+    campaign_name = models.CharField(max_length=255),
+    isOpened = models.BooleanField(),
+    emails_count = models.IntegerField(),
+    date = models.DateField()
+
+class Templates(models.Model):
+    campaigns = models.ForeignKey(Campaigns, on_delete=models.CASCADE),
+    template_name = models.CharField(max_length=255)
+
+
+
+
+
