a18d56a8e064f959daff2457c126bc3c27cc7771
Yaroslav Limanskiy <kobun2341@gmail.com>
2020-04-28 11:14:58 +0300

    importing app into settings + requirements update

--- mailster/mailster/settings.py
+++ mailster/mailster/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'email_import'
+    'email_import',
+    'import_export'
 ]
 
 MIDDLEWARE = [
