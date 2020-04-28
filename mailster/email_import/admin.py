dcc3846e0eb9c8561e0dcb14f20f43a5c0768d74
Yaroslav Limanskiy <kobun2341@gmail.com>
2020-04-28 10:59:22 +0300

    Resource class for Django-import-export

--- mailster/email_import/admin.py
+++ mailster/email_import/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from import_export import resources
+from .models import Contact
 
 # Register your models here.
+class ContactResource(resources.ModelResource):
+    class Meta:
+        model = Contact
+        fields = ('email', 'created_at', 'is_valid', 'campaign_name')
+
