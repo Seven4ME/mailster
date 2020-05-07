dae2a69516051372ba5266008f0171667da7d526
Yaroslav Limanskiy <kobun2341@gmail.com>
2020-05-07 13:26:43 +0300

    url approach change

--- mailster/email_import/urls.py
+++ mailster/email_import/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('dashboard/', dashboard, name="dashboard"),
     path('dashboard/campaigns',login_required(CampaignList.as_view(template_name="companies.html")), name="campaigns_list"),
     path('dashboard/campaigns/create', login_required(CampaignCreate.as_view(template_name="companies_create.html"))),
-    path('dashboard/campaigns/update/<int:pk>/', login_required(CampaignUpdate.as_view(template_name="companies_update.html"))),
+    path('dashboard/campaigns/update/<int:pk>/', login_required(CampaignUpdate.as_view(template_name="companies_update.html")), name="campaigns_update"),
     path('dashboard/contacts', login_required(ContactList.as_view(template_name="contacts.html")), name="contacts_list"),
     path('dashboard/contacts/create', login_required(ContactCreate.as_view(template_name="contacts_create.html")), name="contacts_create"),
     path('dashboard/contacts/update/<int:pk>/', login_required(ContactUpdate.as_view(template_name="contacts_update.html")), name="contacts_update"),
